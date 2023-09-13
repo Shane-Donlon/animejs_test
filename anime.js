836a8594b0fd2a11329c5bd58c61c85be3fd04e4
Shane-Donlon <shane.donlon@live.com>
2023-09-13 13:47:15 +0100

    Name form animation add

--- anime.js
+++ anime.js
@@ -70,3 +70,81 @@ carded.addEventListener("mouseout", () => {
     duration: 1000,
   });
 });
+
+let clock = document.querySelector(".clock");
+let timeSpan = document.querySelector(".time");
+let startBTn = document.querySelector(".start");
+let stopBtn = document.querySelector(".stop");
+
+let time = 25 * 60000;
+timeSpan.textContent = millisToMinutesAndSeconds(time);
+function millisToMinutesAndSeconds(millis) {
+  let minutes = Math.floor(millis / 60000);
+  let seconds = ((millis % 60000) / 1000).toFixed(0);
+  return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+}
+
+let main = millisToMinutesAndSeconds(time);
+
+function timetoZero() {
+  time -= 1000;
+  if (time >= 0) {
+    timeSpan.textContent = millisToMinutesAndSeconds(time);
+  } else clearInterval;
+}
+
+startBTn.addEventListener("click", () => {
+  setInterval(timetoZero, 1000);
+});
+
+let testingDiv = document.querySelector(".testing-div");
+let nameInput = document.querySelector(".name-input");
+let randomInput;
+let submit = document.querySelector(".submit");
+let spanWrapper = document.querySelector(".span-wrapper");
+let duration = 1000;
+submit.addEventListener("click", (e) => {
+  randomInput = nameInput.value;
+  e.preventDefault();
+  let nameValue = nameInput.value.split("");
+  let divContent = document.createElement("div");
+  divContent.classList.add("span-wrapper");
+  testingDiv.appendChild(divContent);
+  nameValue.forEach((char) => {
+    let span = document.createElement("span");
+    divContent.appendChild(span);
+    span.textContent = char;
+    span.classList.add("span-char");
+  });
+  nameInput.value = "";
+  let spanChar = document.querySelectorAll(".span-char");
+
+  var tl = anime.timeline({
+    easing: "easeOutExpo",
+    duration: duration,
+    targets: spanChar,
+    color: function (el, i, l) {
+      colors = ["#4A192C", " #79553D", "#308446", "#20214F", " #999950"];
+      return colors[i];
+    },
+  });
+  tl.add({
+    scale: 3,
+    delay: function (el, i, l) {
+      return i * 100;
+    },
+  });
+
+  tl.add({
+    translateX: 500,
+    delay: function (el, i, l) {
+      return i * 100;
+    },
+  });
+
+  let spanWrapper = document.querySelector(".span-wrapper");
+  setTimeout(() => {
+    spanWrapper.remove();
+    nameInput.value = randomInput;
+  }, duration * 3);
+});
